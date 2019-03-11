abecb99b428d03d8d5db618da09a99d153e1239e
Preston Snugglesworth <17625358+rschroer@users.noreply.github.com>
2019-03-11 18:17:36 -0400

    created vote totals started on output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,8 +6,14 @@ out_file=os.path.join(".","poll_analysis.txt")
 
 candidates=[]
 votes_cast=[]
-weirdostuff=[]
+tally={}
+votecounter=[]
 
+#this function creates the output format
+
+
+
+#read in the file
 with open(in_file, "r") as votes:
     csv_in=csv.reader(votes)
     ##Skip the header ine
@@ -15,8 +21,22 @@ with open(in_file, "r") as votes:
     for ballot in csv_in:
         votes_cast.append(ballot[2])
 
+#calculate the number of total votes
 total_votes=len(votes_cast)
+
+#dedup the candidates and create a dictionary with their names as keys
 candidates=set(votes_cast)
-[weirdostuff.append(vote) for vote in votes_cast if vote=='Khan']
-print(len(candidates))
-print(candidates)
+tally={candidate:0 for candidate in candidates}
+
+#loop through the candidates, count the votes and update the dictionary with their value
+for elector in tally.keys():
+    votecounter.clear()
+    [votecounter.append(vote) for vote in votes_cast if vote==elector]
+    tally[elector]=len(votecounter)
+    
+print(tally)
+
+print(f"Election Results")
+print(f"-------------------")
+for candidate in tally:
+    print(f"{candidate}: {round(tally[candidate]/total_votes*100,3)}")
