b1a496b7d7e7031ab83b70b6225677c1c6471608
Preston Snugglesworth <17625358+rschroer@users.noreply.github.com>
2019-03-13 19:42:15 -0400

    Added comments, added .csv

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -3,10 +3,12 @@ import csv
 import datetime as dt
 
 #Used to determine Runtime
-startime=dt.datetime.now()
+#start_time=dt.datetime.now()
 
+#input and output files
 filename=os.path.join(".","employee_data.csv")
 outputfile=os.path.join(".","modified_employee_data.csv")
+#Initial lists
 Emp_ID=[]
 Name=[]
 SSN=[]
@@ -68,18 +70,20 @@ us_state_abbrev = {
 }
 
 
-
+#This reads the csv file into separate lists
 with open(filename, "r",newline="") as csv_in:
     PII_Data=csv.reader(csv_in, delimiter=",")
     next(PII_Data)
     for row in PII_Data:
         Emp_ID.append(row[0])
         Name.append(row[1])
+        #Date of birth is converted to datetime object
         DOB.append(dt.date.fromisoformat(row[2]))
         SSN.append(row[3])
+        #state is looked up in the dictionary
         state.append(us_state_abbrev[row[4]])
 
-#format things
+#format things and add row headers
 Emp_ID.insert(0,"Emp ID")
 first_name=[name.split(" ")[0] for name in Name]
 first_name.insert(0,"First Nane")
@@ -91,8 +95,13 @@ new_SSN=["***-**-"+sn.split("-")[2] for sn in SSN]
 new_SSN.insert(0,"SSN")
 state.insert(0,"State")
 
+#zip em up and move em out
 outputlist=zip(Emp_ID,first_name,last_name,new_DOB,new_SSN,state)
 
 with open(outputfile, "w") as csv_out:
     writer=csv.writer(csv_out)
     writer.writerows(outputlist)
+
+#this is for QAing runtime
+#runtime=(dt.datetime.now()-start_time).total_seconds()
+#print(runtime)
