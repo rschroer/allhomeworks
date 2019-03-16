99a963f453ad362faca15246ba9e05fc62ededa2
Preston Snugglesworth <17625358+rschroer@users.noreply.github.com>
2019-03-16 14:53:46 -0400

    finished data export and added comments

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,18 +1,25 @@
 import os
-import datetime as dt 
 import re
 
-input_file=os.path.join("raw_data", "paragraph_3.txt") #input("Please type the filename "))
+#user enters the file name
+input_file=os.path.join("raw_data", input("Please type the filename "))
+#initial variables
 paragraph_text=""
 words=[]
 
+#read file
 with open(input_file,"r") as paragraph:
     paragraph_text=paragraph.read().replace('\n', ' ')
 
+#split into sentences, words and letters
 sentences=re.split("(?<=[.!?]) +", paragraph_text)
-words=re.split("(\s+)", paragraph_text)
-[word.remove(" ") for word in words]
+words=re.split(r"\s+", paragraph_text)
+letters=len(paragraph_text)
 
-
-print(len(sentences))
-print(words)
+#this counts the words and prints them out
+print(f"Paragraph Analysis")
+print(f"-----------------")
+print(f"Approximate Word Count: {len(words)}")
+print(f"Approximate Sentence Count: {len(sentences)}")
+print(f"Average Letter Count: {format(letters/len(words),'.2f')}")
+print(f"Average Sentence Length: {format(len(words)/len(sentences),'.2f')}")
